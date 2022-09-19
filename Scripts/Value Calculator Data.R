1b270691042e09352828a812fb1954cc6bced3f6
akcollins32 <56646981+akcollins32@users.noreply.github.com>
2022-09-19 11:42:25 -0400

    Update Value Calculator Data.R

--- Scripts/Value Calculator Data.R
+++ Scripts/Value Calculator Data.R
@@ -4,6 +4,10 @@ require(data.table)
 physician.fees <- fread(file.choose())
 colnames(physician.fees) <- c("Year", "CarrierNumber", "Locality", "HCPCS", "Modifier", "NonFacilityFeeAmt", "FacilityFeeAmt", "PCTC", "StatusCode", "MulitpleSurgery", "TherapyReductionAmt", "TherapyAdjustmentAmtV2", "OPPSIndicator", "OPPSNonFacilityAmt", "OPPSFacilityAmt", "Trailer")
 
+medicare_labs <- fread(file.choose())
+
+
+
 
 emory <- read_json(file.choose())
 
